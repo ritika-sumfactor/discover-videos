8b47e9810a3729053ff8eb2439e638b5368707a9
ritika-sumfactor <129729731+ritika-sumfactor@users.noreply.github.com>
2023-05-23 16:44:02 +0530

    NAV

--- navbar.js
+++ navbar.js
@@ -1,33 +1,42 @@
 import styles from "./navbar.module.css";
 
+import { useRouter } from "next/router";
+import Link from "next/link";
+
 const NavBar = (props) => {
   const { username } = props;
-  return (
-    <div>
-      NavBar
-      <p>{username}</p>
-      <ul>
-        <li>Home</li>
-        <li>My List</li>
-      </ul>
-      <nav>
-        <div>
-          <button>
-            <p>{username}</p>
-          </button>
 
+  const router = useRouter();
+
+  const handleOnClickHome = (e) => {
+    e.preventDefault();
+    router.push("/");
+  };
+
+  const handleOnClickMyList = (e) => {
+    e.preventDefault();
+    router.push("/browse/my-list");
+  };
+
+  return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
         <a className={styles.logoLink}>
           <div className={styles.logoWrapper}>Netflix</div>
         </a>
+
         <ul className={styles.navItems}>
           <li className={styles.navItem}>Home</li>
           <li className={styles.navItem2}>My List</li>
+          <li className={styles.navItem} onClick={handleOnClickHome}>
+            Home
+          </li>
+          <li className={styles.navItem2} onClick={handleOnClickMyList}>
+            My List
+          </li>
         </ul>
         <nav className={styles.navContainer}>
           <div>
-            <a>Sign out</a>
             <button className={styles.usernameBtn}>
               <p className={styles.username}>{username}</p>
             </button>
@@ -35,12 +44,13 @@ const NavBar = (props) => {
             <div className={styles.navDropdown}>
               <div>
                 <a className={styles.linkName}>Sign out</a>
+                <Link href="/login">
+                  <a className={styles.linkName}>Sign out</a>
+                </Link>
                 <div className={styles.lineWrapper}></div>
               </div>
             </div>
           </div>
-        </div>
-      </nav>
         </nav>
       </div>
     </div>

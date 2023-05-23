0ec5d0feda05a6b5304d90405cfadf689f88741d
ritika-sumfactor <129729731+ritika-sumfactor@users.noreply.github.com>
2023-05-23 16:58:16 +0530

    nav

--- navbar.js
+++ navbar.js
@@ -3,13 +3,12 @@ import styles from "./navbar.module.css";
 
 import { useRouter } from "next/router";
 import Link from "next/link";
+import Image from "next/image";
+
 const NavBar = (props) => {
   const { username } = props;
-
   const [showDropdown, setShowDropdown] = useState(false);
-
   const router = useRouter();
-
   const handleOnClickHome = (e) => {
     e.preventDefault();
     router.push("/");
@@ -18,18 +17,25 @@ const NavBar = (props) => {
     e.preventDefault();
     router.push("/browse/my-list");
   };
-
   const handleShowDropdown = (e) => {
     e.preventDefault();
     setShowDropdown(!showDropdown);
   };
-
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
         <a className={styles.logoLink}>
           <div className={styles.logoWrapper}>Netflix</div>
+          <div className={styles.logoWrapper}>
+            <Image
+              src="/static/netflix.svg"
+              alt="Netflix logo"
+              width="128px"
+              height="34px"
+            />
+          </div>
         </a>
+
         <ul className={styles.navItems}>
           <li className={styles.navItem} onClick={handleOnClickHome}>
             Home
@@ -40,17 +46,15 @@ const NavBar = (props) => {
         </ul>
         <nav className={styles.navContainer}>
           <div>
-            <button className={styles.usernameBtn}>
             <button className={styles.usernameBtn} onClick={handleShowDropdown}>
               <p className={styles.username}>{username}</p>
+              <Image
+                src="/static/expand_more.svg"
+                alt="Expand more"
+                width="24px"
+                height="24px"
+              />
             </button>
-
-            <div className={styles.navDropdown}>
-              <div>
-                <Link href="/login">
-                  <a className={styles.linkName}>Sign out</a>
-                </Link>
-                <div className={styles.lineWrapper}></div>
             {showDropdown && (
               <div className={styles.navDropdown}>
                 <div>
@@ -60,7 +64,6 @@ const NavBar = (props) => {
                   <div className={styles.lineWrapper}></div>
                 </div>
               </div>
-            </div>
             )}
           </div>
         </nav>

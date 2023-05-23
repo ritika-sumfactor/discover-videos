ec218f18e16f2585534af5b80c135250e8f79fe4
ritika-sumfactor <129729731+ritika-sumfactor@users.noreply.github.com>
2023-05-23 23:39:25 +0530

    card-components

--- card.js
+++ card.js
@@ -3,8 +3,11 @@ import Image from "next/image";
 import styles from "./card.module.css";
 
 const Card = (props) => {
-  const { imgUrl, size } = props;
   const { imgUrl = "/static/clifford.webp", size = "medium" } = props;
+  const {
+    imgUrl = "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80",
+    size = "medium",
+  } = props;
 
   const [imgSrc, setImgSrc] = useState(imgUrl);
   const classMap = {
@@ -16,6 +19,9 @@ const Card = (props) => {
   const handleOnError = () => {
     console.log("hii error");
     setImgSrc("/static/clifford.webp");
+    setImgSrc(
+      "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80"
+    );
   };
 
   return (
@@ -23,7 +29,6 @@ const Card = (props) => {
       Card
       <div className={classMap[size]}>
         <Image
-          src={imgUrl}
           src={imgSrc}
           alt="image"
           layout="fill"

c328618759f7f4effb115bd557f96fd30d775378
ritika-sumfactor <129729731+ritika-sumfactor@users.noreply.github.com>
2023-05-23 17:02:33 +0530

    pages

---
+++ indexs.js
@@ -0,0 +1,26 @@
+
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Banner from "../components/banner/banner";
+import NavBar from "../components/nav/navbar";
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Netflix</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Netflix</h1>
+
+      <NavBar username="ankita@ank.com" />
+      <Banner
+        title="Clifford the red dog"
+        subTitle="a very cute dog"
+        imgUrl="/static/clifford.webp"
+      />
+      {/*
+      <Card /> */}
+    </div>
+  );
+}

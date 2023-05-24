70f7a17e8c904930908a2ee084ce9f4d2b1c8d45
ritika-sumfactor <129729731+ritika-sumfactor@users.noreply.github.com>
2023-05-24 16:37:52 +0530

    card

--- card.js
+++ card.js
@@ -1,11 +1,8 @@
 import { useState } from "react";
 import Image from "next/image";
-
 import cls from "classnames";
 import { motion } from "framer-motion";
-
 import styles from "./card.module.css";
-
 const Card = (props) => {
   const {
     imgUrl = "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80",
@@ -23,10 +20,10 @@ const Card = (props) => {
       "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80"
     );
   };
+
   return (
     <div className={styles.container}>
       Card
-      <div className={classMap[size]}>
       <motion.div
         className={cls(styles.imgMotionWrapper, classMap[size])}
         whileHover={{ scale: 1.2 }}
@@ -38,7 +35,6 @@ const Card = (props) => {
           onError={handleOnError}
           className={styles.cardImg}
         />
-      </div>
       </motion.div>
     </div>
   );

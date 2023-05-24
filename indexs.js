7ee1361be262f5dfddbdcfe4c42295e5ab6b34e3
ritika-sumfactor <129729731+ritika-sumfactor@users.noreply.github.com>
2023-05-24 16:40:23 +0530

    pages

--- indexs.js
+++ indexs.js
@@ -4,10 +4,17 @@ import Banner from "../components/banner/banner";
 import NavBar from "../components/nav/navbar";
 import Card from "../components/card/card";
 import SectionCards from "../components/card/section-cards";
-
 export default function Home() {
   return (
-export default function Home() {
+    <div className={styles.container}>
+      <Head>
+        <title>Netflix</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <NavBar username="ankita@ank.com" />
+      <Banner
+        title="Clifford the red dog"
+        subTitle="a very cute dog"
         imgUrl="/static/clifford.webp"
       />
 

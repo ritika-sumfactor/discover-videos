e0ac7fa35bb1cb5dcac8398265b413636df76fcf
ritika-sumfactor <129729731+ritika-sumfactor@users.noreply.github.com>
2023-05-25 17:16:08 +0530

    pages

--- login.js
+++ login.js
@@ -1,33 +1,39 @@
 import { useState } from "react";
+
+import { useRouter } from "next/router";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-
 import styles from "../styles/Login.module.css";
-
 const Login = () => {
   const [email, setEmail] = useState("");
   const [userMsg, setUserMsg] = useState("");
+
+  const router = useRouter();
+
   const handleOnChangeEmail = (e) => {
     setUserMsg("");
     console.log("event", e);
     const email = e.target.value;
     setEmail(email);
   };
-
   const handleLoginWithEmail = async (e) => {
-    e.preventDefault();
     console.log("hi button");
     e.preventDefault();
 
     if (email) {
       // route to dashboard
+      if (email === "kulkarni.ankita09@gmail.com") {
+        // route to dashboard
+        router.push("/");
+      } else {
+        console.log("Something went wrong logging in");
+      }
     } else {
       // show user message
       setUserMsg("Enter a valid email address");
     }
   };
-
   return (
     <div className={styles.container}>
       <Head>
@@ -58,8 +64,81 @@ const Login = () => {
             className={styles.emailInput}
             onChange={handleOnChangeEmail}
           />
+          <p className={styles.userMsg}>{userMsg}</p>
+          <button onClick={handleLoginWithEmail} className={styles.loginBtn}>
+            Sign In
+          </button>
+        </div>
+      </main>
+    </div>
+  );
+};
+export default Login;import { useState } from "react";
 
-          <p className={styles.userMsg}></p>
+import { useRouter } from "next/router";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../styles/Login.module.css";
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [userMsg, setUserMsg] = useState("");
+
+  const router = useRouter();
+
+  const handleOnChangeEmail = (e) => {
+    setUserMsg("");
+    console.log("event", e);
+    const email = e.target.value;
+    setEmail(email);
+  };
+  const handleLoginWithEmail = async (e) => {
+    console.log("hi button");
+    e.preventDefault();
+
+    if (email) {
+      // route to dashboard
+      if (email === "kulkarni.ankita09@gmail.com") {
+        // route to dashboard
+        router.push("/");
+      } else {
+        console.log("Something went wrong logging in");
+      }
+    } else {
+      // show user message
+      setUserMsg("Enter a valid email address");
+    }
+  };
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Netflix SignIn</title>
+      </Head>
+      <header className={styles.header}>
+        <div className={styles.headerWrapper}>
+          <Link className={styles.logoLink} href="/">
+            <a>
+              <div className={styles.logoWrapper}>
+                <Image
+                  src="/static/netflix.svg"
+                  alt="Netflix logo"
+                  width="128px"
+                  height="34px"
+                />
+              </div>
+            </a>
+          </Link>
+        </div>
+      </header>
+      <main className={styles.main}>
+        <div className={styles.mainWrapper}>
+          <h1 className={styles.signinHeader}>Sign In</h1>
+          <input
+            type="text"
+            placeholder="Email address"
+            className={styles.emailInput}
+            onChange={handleOnChangeEmail}
+          />
           <p className={styles.userMsg}>{userMsg}</p>
           <button onClick={handleLoginWithEmail} className={styles.loginBtn}>
             Sign In

f830f0cb89479586ba079bbb942eeec281ddedc6
ritika-sumfactor <129729731+ritika-sumfactor@users.noreply.github.com>
2023-05-29 12:04:19 +0530

    videos

--- [videoId].js
+++ [videoId].js
@@ -1,23 +1,44 @@
 import { useRouter } from "next/router";
 import Modal from "react-modal";
 import styles from "../../styles/Video.module.css";
+
+import NavBar from "../../components/nav/navbar";
 import clsx from "classnames";
+import { getYoutubeVideoById } from "../../lib/videos";
+
 Modal.setAppElement("#__next");
-const Video = () => {
+export async function getStaticProps(context) {
+  console.log({ context });
+  const videoId = context.params.videoId;
+  const videoArray = await getYoutubeVideoById(videoId);
+  return {
+    props: {
+      video: videoArray.length > 0 ? videoArray[0] : {},
+    },
+    revalidate: 10, // In seconds
+  };
+}
+export async function getStaticPaths() {
+  const listOfVideos = ["mYfJxlgR2jw", "4zH5iYM4wJo", "KCPEHsAViiQ"];
+  const paths = listOfVideos.map((videoId) => ({
+    params: { videoId },
+  }));
+  return { paths, fallback: "blocking" };
+}
+const Video = ({ video }) => {
   const router = useRouter();
   console.log({ router });
-  const video = {
-    title: "Hi cute dog",
-    publishTime: "1990-01-01",
-    description: "A big red dog that is super cute, can he get any bigger?",
-    description:
-      "A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger?",
-    channelTitle: "Paramount Pictures",
-    viewCount: 10000,
-  };
-  const { title, publishTime, description, channelTitle, viewCount } = video;
+  const {
+    title,
+    publishTime,
+    description,
+    channelTitle,
+    statistics: { viewCount } = { viewCount: 0 },
+  } = video;
+
   return (
     <div className={styles.container}>
+      <NavBar />
       <Modal
         isOpen={true}
         contentLabel="Watch the video"
--- videos.js
+++ videos.js
@@ -1,24 +1,48 @@
 import videoData from "../data/videos.json";
+import videoTestData from "../data/videos.json";
+
 export const getCommonVideos = async (url) => {
+const fetchVideos = async (url) => {
   const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
+  const BASE_URL = "youtube.googleapis.com/youtube/v3";
+
   try {
     const BASE_URL = "youtube.googleapis.com/youtube/v3";
     const response = await fetch(
       `https://${BASE_URL}/${url}&maxResults=25&key=${YOUTUBE_API_KEY}`
     );
+  const response = await fetch(
+    `https://${BASE_URL}/${url}&maxResults=25&key=${YOUTUBE_API_KEY}`
+  );
+
     const data = await response.json();
+  return await response.json();
+};
+
+export const getCommonVideos = async (url) => {
+  try {
+    const isDev = process.env.DEVELOPMENT;
+    console.log({ isDev });
+    const data = isDev ? videoTestData : await fetchVideos(url);
     if (data?.error) {
       console.error("Youtube API error", data.error);
       return [];
     }
+
     console.log({ items: data.items });
+
     return data?.items.map((item) => {
       console.log({ id: item.id });
       const id = item.id?.videoId || item.id;
+      const snippet = item.snippet;
       return {
-        title: item.snippet.title,
+        title: snippet?.title,
         imgUrl: item.snippet.thumbnails.high.url,
         id,
+        description: snippet.description,
+        publishTime: snippet.publishedAt,
+        channelTitle: snippet.channelTitle,
+        statistics: item.statistics ? item.statistics : { viewCount: 0 },
       };
     });
   } catch (error) {
@@ -33,7 +57,10 @@ export const getVideos = (searchQuery) => {
 export const getPopularVideos = () => {
   const URL =
     "videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US";
-
   //videos?part=snippet%2CcontentDetails%2Cstatistics&id=Ks-_Mh1QhMc
+  return getCommonVideos(URL);
+};
+export const getYoutubeVideoById = (videoId) => {
+  const URL = `videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}`;
   return getCommonVideos(URL);
 };

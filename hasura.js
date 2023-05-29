513b4500881487113eb67337128e449276535e4e
ritika-sumfactor <129729731+ritika-sumfactor@users.noreply.github.com>
2023-05-29 16:33:35 +0530

    Lib

---
+++ hasura.js
@@ -0,0 +1,36 @@
+async function queryHasuraGQL(operationsDoc, operationName, variables) {
+    const result = await fetch(process.env.NEXT_PUBLIC_HASURA_ADMIN_URL, {
+      method: "POST",
+      headers: {
+        "x-hasura-admin-secret": process.env.NEXT_PUBLIC_HASURA_ADMIN_SECRET,
+        Authorization: "Bearer <token>",
+      },
+      body: JSON.stringify({
+        query: operationsDoc,
+        variables: variables,
+        operationName: operationName,
+      }),
+    });
+    return await result.json();
+  }
+  function fetchMyQuery() {
+    const operationsDoc = `
+      query MyQuery {
+        users {
+          email
+          id
+          issuer
+          publicAddress
+        }
+      }
+    `;
+    return queryHasuraGQL(operationsDoc, "MyQuery", {});
+  }
+  export async function startFetchMyQuery() {
+    const { errors, data } = await fetchMyQuery();
+    if (errors) {
+      console.error(errors);
+    }
+    console.log(data);
+  }
+  startFetchMyQuery();

d4802ae17a23c30d1e9666154a652dfc031789c3
ritika-sumfactor <129729731+ritika-sumfactor@users.noreply.github.com>
2023-05-30 17:19:13 +0530

    components

---
+++ navbar.js
@@ -0,0 +1,98 @@
+import { useState, useEffect } from "react";
+import styles from "./navbar.module.css";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import Image from "next/image";
+import { magic } from "../../lib/magic-client";
+const NavBar = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [username, setUsername] = useState("");
+  const router = useRouter();
+  useEffect(() => {
+    async function getUsername() {
+      try {
+        const { email } = await magic.user.getMetadata();
+        const { email, issuer } = await magic.user.getMetadata();
+        const didToken = await magic.user.getIdToken();
+        console.log({ didToken });
+        if (email) {
+          setUsername(email);
+        }
+      } catch (error) {
+        console.log("Error retrieving email:", error);
+      }
+    }
+    getUsername();
+  }, []);
+  const handleOnClickHome = (e) => {
+    e.preventDefault();
+    router.push("/");
+  };
+  const handleOnClickMyList = (e) => {
+    e.preventDefault();
+    router.push("/browse/my-list");
+  };
+  const handleShowDropdown = (e) => {
+    e.preventDefault();
+    setShowDropdown(!showDropdown);
+  };
+  const handleSignout = async (e) => {
+    e.preventDefault();
+    try {
+      await magic.user.logout();
+      console.log(await magic.user.isLoggedIn());
+      router.push("/login");
+    } catch (error) {
+      console.error("Error logging out", error);
+      router.push("/login");
+    }
+  };
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <a className={styles.logoLink}>
+          <div className={styles.logoWrapper}>
+            <Image
+              src="/static/netflix.svg"
+              alt="Netflix logo"
+              width="128px"
+              height="34px"
+            />
+          </div>
+        </a>
+        <ul className={styles.navItems}>
+          <li className={styles.navItem} onClick={handleOnClickHome}>
+            Home
+          </li>
+          <li className={styles.navItem2} onClick={handleOnClickMyList}>
+            My List
+          </li>
+        </ul>
+        <nav className={styles.navContainer}>
+          <div>
+            <button className={styles.usernameBtn} onClick={handleShowDropdown}>
+              <p className={styles.username}>{username}</p>
+              <Image
+                src="/static/expand_more.svg"
+                alt="Expand more"
+                width="24px"
+                height="24px"
+              />
+            </button>
+            {showDropdown && (
+              <div className={styles.navDropdown}>
+                <div>
+                  <a className={styles.linkName} onClick={handleSignout}>
+                    Sign out
+                  </a>
+                  <div className={styles.lineWrapper}></div>
+                </div>
+              </div>
+            )}
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+};
+export default NavBar;

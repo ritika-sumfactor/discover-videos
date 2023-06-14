d9e8998ebe60ebe8a0a7f7676e04f6787b2ec290
ritika-sumfactor <129729731+ritika-sumfactor@users.noreply.github.com>
2023-06-14 07:00:55 +0530

    login.js

--- login.js
+++ login.js
@@ -1,53 +1,117 @@
-import { magicAdmin } from "../../lib/magic";
-import jwt from "jsonwebtoken";
-import { createNewUser, isNewUser } from "../../lib/db/hasura";
-import { setTokenCookie } from "../../lib/cookies";
-export default async function login(req, res) {
-  if (req.method === "POST") {
-    try {
-      const auth = req.headers.authorization;
-      const didToken = auth ? auth.substr(7) : "";
-      const metadata = await magicAdmin.users.getMetadataByToken(didToken);
-      console.log({ metadata });
-      const token = jwt.sign(
-        {
-          ...metadata,
-          iat: Math.floor(Date.now() / 1000),
-          exp: Math.floor(Date.now() / 1000 + 7 * 24 * 60 * 60),
-          "https://hasura.io/jwt/claims": {
-            "x-hasura-allowed-roles": ["user", "admin"],
-            "x-hasura-default-role": "user",
-            "x-hasura-user-id": `${metadata.issuer}`,
-          },
-        },
-        process.env.JWT_SECRET
-      );
-      console.log({ token });
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../styles/Login.module.css";
+import { magic } from "../lib/magic-client";
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [userMsg, setUserMsg] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+  useEffect(() => {
+    const handleComplete = () => {
+      setIsLoading(false);
+    };
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+    return () => {
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
+  }, [router]);
+  const handleOnChangeEmail = (e) => {
+    setUserMsg("");
+    console.log("event", e);
+    const email = e.target.value;
+    setEmail(email);
+  };
+  const handleLoginWithEmail = async (e) => {
+    console.log("hi button");
+    e.preventDefault();
+    setIsLoading(true);
+    if (email) {
+      if (email === "kulkarni.ankita09@gmail.com") {
+        //  log in a user by their email
+        try {
+          const didToken = await magic.auth.loginWithMagicLink({
+            email,
+          });
+          console.log({ didToken });
+          if (didToken) {
+            setIsLoading(false);
+            router.push("/");
+            const response = await fetch("/api/login", {
+              method: "POST",
+              headers: {
+                Authorization: `Bearer ${didToken}`,
+                "Content-Type": "application/json",
+              },
+            });
 
-      // CHECK IF USER EXISTS
-
-      const isNewUserQuery = await isNewUser(token, metadata.issuer);
-      if (isNewUserQuery) {
-        const createNewUserMutation = await createNewUser(token, metadata);
-        console.log({ createNewUserMutation });
-        //set the cookie
-        const cookie = setTokenCookie(token);
-        console.log({ cookie });
-        res.send({ done: true, msg: "is a new user" });
+            const loggedInResponse = await response.json();
+            if (loggedInResponse.done) {
+              router.push("/");
+            } else {
+              setIsLoading(false);
+              setUserMsg("Something went wrong logging in");
+            }
+          }
+        } catch (error) {
+          // Handle errors if required!
+          console.error("Something went wrong logging in", error);
+          setIsLoading(false);
+        }
       } else {
-        //set the cookie
-        const cookie = setTokenCookie(token);
-        console.log({ cookie });
-        res.send({ done: true, msg: "not a new user" });
+        setIsLoading(false);
+        setUserMsg("Something went wrong logging in");
       }
-      isNewUserQuery && (await createNewUser(token, metadata));
-      setTokenCookie(token, res);
-      res.send({ done: true });
-    } catch (error) {
-      console.error("Something went wrong logging in", error);
-      res.status(500).send({ done: false });
+    } else {
+      // show user message
+      setIsLoading(false);
+      setUserMsg("Enter a valid email address");
     }
-  } else {
-    res.send({ done: false });
+  };
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Netflix SignIn</title>
+      </Head>
+      <header className={styles.header}>
+        <div className={styles.headerWrapper}>
+          <Link className={styles.logoLink} href="/">
+            <a>
+              <div className={styles.logoWrapper}>
+                <Image
+                  src="/static/netflix.svg"
+                  alt="Netflix logo"
+                  width="128px"
+                  height="34px"
+                />
+              </div>
+            </a>
+          </Link>
+        </div>
+      </header>
+      <main className={styles.main}>
+        <div className={styles.mainWrapper}>
+          <h1 className={styles.signinHeader}>Sign In</h1>
+          <input
+            type="text"
+            placeholder="Email address"
+            className={styles.emailInput}
+            onChange={handleOnChangeEmail}
+          />
+          <p className={styles.userMsg}>{userMsg}</p>
+          <button onClick={handleLoginWithEmail} className={styles.loginBtn}>
+            {isLoading ? "Loading..." : "Sign In"}
+          </button>
+        </div>
+      </main>
+    </div>
+  );
+};
+export default Login;
   }
 }

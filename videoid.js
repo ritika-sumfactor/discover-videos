b1a4c9982ef6c196a5cab4dc92b4c6dd3dfa36c5
ritika-sumfactor <129729731+ritika-sumfactor@users.noreply.github.com>
2023-06-15 09:25:29 +0530

    Pages

--- videoid.js
+++ videoid.js
@@ -1,17 +1,13 @@
+import { useState } from "react";
 import { useRouter } from "next/router";
 import Modal from "react-modal";
 import styles from "../../styles/Video.module.css";
-
 import NavBar from "../../components/nav/navbar";
 import clsx from "classnames";
-
 import { getYoutubeVideoById } from "../../lib/videos";
-
 import Like from "../../components/icons/like-icon";
 import DisLike from "../../components/icons/dislike-icon";
-
 Modal.setAppElement("#__next");
-
 export async function getStaticProps(context) {
   const videoId = context.params.videoId;
   const videoArray = await getYoutubeVideoById(videoId);
@@ -29,9 +25,14 @@ export async function getStaticPaths() {
   }));
   return { paths, fallback: "blocking" };
 }
+
 const Video = ({ video }) => {
   const router = useRouter();
   console.log({ router });
+  const videoId = router.query.videoId;
+
+  const [toggleLike, setToggleLike] = useState(false);
+  const [toggleDisLike, setToggleDisLike] = useState(false);
   const {
     title,
     publishTime,
@@ -39,6 +40,45 @@ const Video = ({ video }) => {
     channelTitle,
     statistics: { viewCount } = { viewCount: 0 },
   } = video;
+  const handleToggleDislike = async () => {
+    console.log("handleToggleDislike");
+    setToggleDisLike(!toggleDisLike);
+    setToggleLike(toggleDisLike);
+
+    const val = !toggleDisLike;
+
+    const response = await fetch("/api/stats", {
+      method: "POST",
+      body: JSON.stringify({
+        videoId,
+        favourited: val ? 0 : 1,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    console.log("data", await response.json());
+  };
+
+  const handleToggleLike = async () => {
+    console.log("handleToggleLike");
+    const val = !toggleLike;
+    setToggleLike(!toggleLike);
+    setToggleDisLike(toggleLike);
+
+    const response = await fetch("/api/stats", {
+      method: "POST",
+      body: JSON.stringify({
+        videoId,
+        favourited: val ? 1 : 0,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    console.log("data", await response.json());
+  };
+
   return (
     <div className={styles.container}>
       <NavBar />
@@ -56,22 +96,24 @@ const Video = ({ video }) => {
           width="100%"
           height="360"
           src={`https://www.youtube.com/embed/${router.query.videoId}?autoplay=0&origin=http://example.com&controls=0&rel=1`}
+          src={`https://www.youtube.com/embed/${videoId}?autoplay=0&origin=http://example.com&controls=0&rel=1`}
           frameBorder="0"
         ></iframe>
 
         <div className={styles.likeDislikeBtnWrapper}>
-          <div className={styles.btnWrapper}>
-            <button>
-              <Like />
+          <div className={styles.likeBtnWrapper}>
+            <button onClick={handleToggleLike}>
+              <div className={styles.btnWrapper}>
+                <Like selected={toggleLike} />
+              </div>
             </button>
           </div>
-          <div className={styles.btnWrapper}>
-            <button>
-              <DisLike />
-            </button>
-          </div>
+          <button onClick={handleToggleDislike}>
+            <div className={styles.btnWrapper}>
+              <DisLike selected={toggleDisLike} />
+            </div>
+          </button>
         </div>
-
         <div className={styles.modalBody}>
           <div className={styles.modalBodyContent}>
             <div className={styles.col1}>
